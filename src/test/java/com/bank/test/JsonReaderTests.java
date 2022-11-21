95375c7a39a0fbaed0df2bd485c335c911045e32
wryther <rytherwy@mail.uc.edu>
2022-11-20 20:27:30 -0500

    Adding possible tests for JSON parser class and excluding

--- src/test/java/com/bank/test/JsonReaderTests.java
+++ src/test/java/com/bank/test/JsonReaderTests.java
@@ -1,7 +1,27 @@
 package com.bank.test;
 
 import com.bank.JsonReader;
+import org.junit.jupiter.api.Test;
+
+import static com.bank.JsonReader.parseAccountObject;
+import static com.bank.JsonReader.readAccounts;
 
 public class JsonReaderTests {
+
+    //Given json test file, read file
+    @Test
+    private void givenJsonTestAccountFile() {
+        readAccounts();
+    }
+
+    @Test
+    private void whenCreateAccountFromJsonTestAccount() {
+        parseAccountObject();
+    }
+
+    @Test
+    private void thenJsonTestAccountKeyValuePairsShouldBeAccountProperties() {
+
+    }
 
 }
