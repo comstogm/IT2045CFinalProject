f38319f76c66b86d7201de52342fc33272fe5e46
wryther <rytherwy@mail.uc.edu>
2022-11-28 00:09:43 -0500

    Updated jsonReader tests to better reflect class

--- src/test/java/com/bank/test/JsonReaderTests.java
+++ src/test/java/com/bank/test/JsonReaderTests.java
@@ -5,24 +5,28 @@ import org.junit.jupiter.api.Test;
 
 import static com.bank.JsonReader.parseAccountObject;
 import static com.bank.JsonReader.readAccounts;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonReaderTests {
 
     final String FILE = "testAccounts.json";
 
+    @Test
+    public void create_ObjectFromtestAccountsjsonFile() {
+       givenJsonTestAccountFileAndWhenCreateAccountFromJsonTestAccount();
+       thenJsonTestAccountKeyValuePairsShouldBeAccountProperties();
+    }
+
     //Given json test file, read file
-    private void givenJsonTestAccountFile() {
+    private void givenJsonTestAccountFileAndWhenCreateAccountFromJsonTestAccount() {
         readAccounts(FILE);
     }
 
-    @Test
-    private void whenCreateAccountFromJsonTestAccount() {
-        //parseAccountObject();
-    }
-
-    @Test
     private void thenJsonTestAccountKeyValuePairsShouldBeAccountProperties() {
-
+        assertEquals("Checking", readAccounts.get(0).getClass());
+        assertEquals(5.0, readAccounts.get(0).getInterest());
+        assertEquals(10000.0, readAccounts.get(0).getBalance());
+        assertEquals(10, readAccounts.get(0).getPeriods());
     }
 
 }
